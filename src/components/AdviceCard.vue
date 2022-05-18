d13c08782872e7118e5b9d24d4b12177d0aa818d
realabdullah <abdulodesanmi@gmail.com>
2022-05-18 18:59:49 +0100

    first commit

---
+++ src/components/AdviceCard.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="__card">
+      <p class="advice__no"></p>
+      <p class="advice__text"></p>
+      <!-- Divider goes here -->
+      <!-- Dice goes here -->
+    </div>
+  </div>
+</template>
