c1bcb47050376a223ac0ad8419343c4bef19921e
realabdullah <abdulodesanmi@gmail.com>
2022-06-03 05:58:45 +0100

    data fetch

--- src/components/AdviceCard.vue
+++ src/components/AdviceCard.vue
@@ -2,26 +2,26 @@
 import PatternDividerD from './icons/PatternDividerD.vue';
 import IconDice from './icons/IconDice.vue';
 import PatternDividerM from './icons/PatternDividerM.vue';
-import { storeToRefs } from 'pinia'
+import { onBeforeMount } from 'vue'
 import { useGetQuote } from '../stores/getQuote'
 
-const { quote } = storeToRefs(useGetQuote())
-const { getQuote } = useGetQuote()
+const quote = useGetQuote()
 
-getQuote()
-
+onBeforeMount(() => {
+  quote.getQuote()
+})
 </script>
 
 <template>
   <div class="wrapper">
     <div class="__card">
-      <p class="advice__no">advice #{{quote.slip.id}}</p>
+      <p class="advice__no">advice #{{quote.id}}</p>
       <p class="advice__text">
-        "{{ quote.slip.advice }}"
+        "{{ quote.quote }}"
       </p>
       <PatternDividerD />
       <PatternDividerM />
-      <div class="dice__div" @click="getQuote">
+      <div class="dice__div" @click="quote.getQuote">
         <IconDice />
       </div>
     </div>
--- src/stores/getQuote.js
+++ src/stores/getQuote.js
@@ -3,14 +3,18 @@ import { defineStore } from "pinia"
 export const useGetQuote = defineStore({
   id: "getQuote",
   state: () => ({
-    quote: []
+    id: null,
+    quoteData: null,
+    quote: null
   }),
 
   actions: {
     async getQuote() {
       try {
-        this.quote = await fetch("	https://api.adviceslip.com/advice")
+        this.quoteData = await fetch("https://api.adviceslip.com/advice")
         .then((response) => response.json())
+        this.id = this.quoteData.slip.id
+        this.quote = this.quoteData.slip.advice
       } catch (error) {
         // console.log(error)
       }
